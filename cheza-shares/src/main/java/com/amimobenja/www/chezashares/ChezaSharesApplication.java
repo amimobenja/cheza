8780a6dba21116b38f9747116b84d35aae592615
Amimo Benja <amimobenja08@gmail.com>
2019-04-03 07:48:31 +0300

    1st Commit

---
+++ cheza-shares/src/main/java/com/amimobenja/www/chezashares/ChezaSharesApplication.java
@@ -0,0 +1,13 @@
+package com.amimobenja.www.chezashares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChezaSharesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChezaSharesApplication.class, args);
+	}
+
+}
